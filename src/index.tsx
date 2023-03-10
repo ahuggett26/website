fc63a39fa0494f8cfeb113c474783b22e3aad0b4
ahuggett26 <ahuggett@hotmail.co.uk>
2023-03-10 00:32:29 +0000

    Use react-router
    Uses react-router-dom to enable navigation to different paths

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './components/Home';
+import Home from './components/Home';
+import Relaxation from './components/Relaxtion';
 import reportWebVitals from './reportWebVitals';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home/>
+  },
+  {
+    path: "apps/relaxation",
+    element: <Relaxation/>
+  }
+])
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
