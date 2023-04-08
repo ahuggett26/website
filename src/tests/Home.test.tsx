59a988d3da5a402034ba00af001f34f7a8534078
ahuggett26 <ahuggett@hotmail.co.uk>
2023-04-08 17:21:57 +0100

    fix tests
    fix for error found https://stackoverflow.com/questions/75728532/uncaught-typeerror-cannot-destructure-property-basename-of-react2-usecontext. It would be good to get rid of the BrowserRouter somehow, so I don't have to add this everywhere where there are links...

--- src/tests/Home.test.tsx
+++ src/tests/Home.test.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import Home from '../components/home/Home';
+import { BrowserRouter } from 'react-router-dom';
 
-test('renders coming soon text', () => {
-  render(<Home />);
-  const comingSoonElem = screen.getByText(/Coming soon/i);
-  expect(comingSoonElem).toBeInTheDocument();
+test('renders header title', () => {
+  render(
+    <BrowserRouter>
+      <Home />
+    </BrowserRouter>
+  );
+  const headerTitle = screen.getByText(/ahuggett.uk/i);
+  expect(headerTitle).toBeInTheDocument();
 });
